3a24bef586a94da9aedc7ee7751cd691f23d24f2
dihauet <dimitrihauet@gmail.com>
2022-02-28 09:03:36 +0100

    first commit

---
+++ src/components/customSlider/customSlider.style.js
@@ -0,0 +1,16 @@
+import { styled } from '@mui/material/styles';
+import Slider from '@mui/material/Slider';
+
+const CustomizedSlider = styled(Slider)`
+  color: #20b2aa;
+
+  :hover {
+    color: #2e8b57;
+  }
+
+  & .MuiSlider-thumb {
+    border-radius: 1px;
+  }
+`;
+
+export default CustomizedSlider
